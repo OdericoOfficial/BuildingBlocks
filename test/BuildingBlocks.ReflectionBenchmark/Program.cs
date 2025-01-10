d6143303ec2c816574555b2d688925165ff9b072
Orderico <2919128969@qq.com>
2025-01-10 14:50:46 +0800

    no message

--- test/BuildingBlocks.ReflectionBenchmark/Program.cs
+++ test/BuildingBlocks.ReflectionBenchmark/Program.cs
@@ -1,25 +1,10 @@
-﻿using BenchmarkDotNet.Columns;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Loggers;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
 namespace BuildingBlocks.ReflectionBenchmark
 {
     internal class Program
     {
-        internal class MinimalBenchmarkConfig : ManualConfig
-        {
-            public static MinimalBenchmarkConfig Instance { get; } = new MinimalBenchmarkConfig();
-
-            private MinimalBenchmarkConfig()
-            {
-                AddLogger(ConsoleLogger.Unicode);
-                AddColumnProvider(DefaultColumnProviders.Instance);
-                AddExporter(DefaultExporters.Csv);
-            }
-        }
         static void Main(string[] args)
-            => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, MinimalBenchmarkConfig.Instance);
+            => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
     }
 }
